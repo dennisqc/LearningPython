592e16c274691928b66ffab6785672c37c82366d
dennisquispe72 <dennisquispe72@gmail.com>
2024-03-23 20:50:18 -0500

    first commit

---
+++ Modulo 5/operadores.py
@@ -0,0 +1,20 @@
+# miVariable = 10
+# print(miVariable)
+
+# miVariable = miVariable +1
+# print(miVariable)
+
+
+# a = 5
+# b = 6
+# resultado = (a == b)
+# print(f'Resultado == : {resultado}')
+
+a = int(input('Escribe un valor numerico:'))
+
+if a % 2 == 0:
+    print(f'El valor de a {a} es numero par')
+
+else:
+    print(f'El valor de a {a} es numero impar')
+
---
+++ Modulo 5/operadores3.py
@@ -0,0 +1,7 @@
+number1 = int(input('Ingrese el numero1:'))
+number2 = int(input('Ingrese el numero2:'))
+
+if number1 > number2:
+    print(f'El numero1 mayor es : {number1}')
+else:
+    print(f'El numero2 mayor es : {number2}')
